a76651c7e01d9f4b08e0e988649d774b4fd25e69
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-06-08 15:03:48 +0530

    Add localhost too for origin

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://astonishing-marzipan-0a9f8d.netlify.app',
+  origin: ['https://astonishing-marzipan-0a9f8d.netlify.app', "http://127.0.0.1:5173"],
   credentials: true
 }));
 app.use(express.json());
