05ed5862c88b20eb33eca7e07595b8569359a79b
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-06-08 14:20:19 +0530

    update origin in cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://astonishing-marzipan-0a9f8d.netlify.app/',
+  origin: 'https://astonishing-marzipan-0a9f8d.netlify.app',
   credentials: "true"
 }));
 app.use(express.json());
