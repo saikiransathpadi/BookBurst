77682c099b3a20125e63b0b064082390b7b652dd
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-06-08 14:24:00 +0530

    update creds to bool

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: 'https://astonishing-marzipan-0a9f8d.netlify.app',
-  credentials: "true"
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
