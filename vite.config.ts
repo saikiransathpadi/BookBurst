877529aedcc9a32abc1bb8ae5d4cde6010a737dc
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-06-08 13:04:57 +0530

    Update BE url

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://bookburst-aoqq.onrender.com/',
         changeOrigin: true
       }
     }
